f7db7ea94b5d957e9e4e2de310a5019edbcaca3c
dennisDev77 <diniscamela5@gmail.com>
2025-03-13 20:28:56 +0100

    feat(App.jsx) Add bg color

--- controllers/App.jsx
+++ controllers/App.jsx
@@ -6,7 +6,9 @@ import Routes from './Routes'
 export default function () {
     return(
      <>
+     <main className='bg-color_gray_500 h-lvh w-full'>
      <Routes/>
+     </main>
     </>
     )
 }
