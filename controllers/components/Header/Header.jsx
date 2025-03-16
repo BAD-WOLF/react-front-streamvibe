4dd31a27435754e27003a9177b626d8f397cd698
BAD-WOLF <matheusviaira160@gmail.com>
2025-03-16 11:49:52 +0100

    Add component Header

--- controllers/components/Header/Header.jsx
+++ controllers/components/Header/Header.jsx
@@ -1,21 +1,40 @@
 import React from 'react'
-import Link from 'react-router'
+import {Link } from 'react-router'
+import './Header.css'
+import {FaUser} from 'react-icons/fa'
+
+//import the component
+import Logo from '../Logo/Logo'
+import Button from '../Forms/Button'
 
 export default function Header(){
+
     return(
         <>
-            <header className='bg-gray-800'>
-                <section className='Logo'>
-                    <Link>IMBD2</Link>
+        <main className={`bg-color_gray_500 sticky top-0`}>
+            <header className={`flex justify-between items-center py-2 px-4`}>
+
+                <section>
+                    <Logo/>
                 </section>
 
-                <nav className='nav'>
-                <Link>Films</Link>
-                <Link>Series</Link>
-                <Link>Ajuda</Link>
-                <Link>Suporte</Link>
+                <nav className={` text-gray-200 flex justify-start items-center gap-2 text-base `}>
+                    <Link to='/' className={` font-medium hover:font-normal hover:text-gray-300 transition-all`}>Films</Link>
+                    <Link to='/' className='font-medium hover:font-normal hover:text-gray-300 transition-all'>Series</Link>
+                    <Link to='/' className='font-medium hover:font-normal hover:text-gray-300 transition-all'>Populares</Link>
+                    <Link to='/' className='font-medium hover:font-normal hover:text-gray-300 transition-all'>Suporte</Link>
+                    <Link to='/' className='font-medium hover:font-normal hover:text-gray-300 transition-all'>Contactos</Link>
                 </nav>
+
+                <section>
+                <Button>
+                    <span><FaUser/></span>
+                    <span>Minha Conta</span>
+                </Button>
+                </section>
+
             </header>
+        </main>
         </>
     )
 }
