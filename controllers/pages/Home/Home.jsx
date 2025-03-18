1b1c98b34a916891209b48555aea5f78f2af0c5b
dennisDev77 <diniscamela5@gmail.com>
2025-03-18 21:22:26 +0100

    Add text to testimonials

--- controllers/pages/Home/Home.jsx
+++ controllers/pages/Home/Home.jsx
@@ -13,11 +13,11 @@ export default function Home(){
     const testimonials = [
         { quote: "Uma experiência cinematográfica incrível! Cada filme e série  aqui me surpreenderam. Não perco mais tempo escolhendo o que assistir!", name: "Dennis Camela", designation: "Visitante", src:dennis },
 
-        { quote: "Se você ama filmes e séries tanto quanto eu, esse site é perfeito! As análises e sugestões são sempre certeiras!", name: "Goreth Antonio", designation: "Visitante", src: goreth },
+        { quote: "Se você ama filmes e séries tanto quanto eu, esse site é perfeito! As análises e sugestões são sempre certeiras!", name: "Goreth Antonio", designation: "Atriz", src: goreth },
 
-        { quote: "Uma experiência cinematográfica incrível! Cada filme e série  aqui me surpreenderam. Não perco mais tempo escolhendo o que assistir!", name: "Laurinda Andre", designation: "Visitante", src:lau },
+        { quote: "Uma experiência cinematográfica incrível! Cada filme e série  aqui me surpreenderam. Não perco mais tempo escolhendo o que assistir!", name: "Laumara Andre", designation: "Visitante", src:lau },
 
-        { quote: "Se você ama filmes e séries tanto quanto eu, esse site é perfeito! As análises e sugestões são sempre certeiras!", name: "Maria Paulin", designation: "Visitante", src: maria },
+        { quote: "Se você ama filmes e séries tanto quanto eu, esse site é perfeito! As análises e sugestões são sempre certeiras!", name: "Maria Paulin", designation: "Atriz", src: maria },
         
         { quote: "Se você ama filmes e séries tanto quanto eu, esse site é perfeito! As análises e sugestões são sempre certeiras!", name: "Matheu Joao", designation: "Visitante", src: mathe },
         
@@ -33,7 +33,7 @@ export default function Home(){
             </section>            
 
             {/* Show the films and serie */}
-            <section className='py-8'>
+            <section className='py-4'>
                 <Depoimento testimonials={testimonials} autoplay/>
             </section>
         </main>
