66eac3e81b15786ca0e047749b2716a6ae021a07
BAD-WOLF <matheusviaira160@gmail.com>
2025-03-18 19:20:36 +0100

    Change the way to app.css

--- controllers/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Home(){
-     return (
-        <>
-         <h1 className='text-4xl text-blue-700'>Bem Vindo a Home</h1>
-        </>
-     )
-}
