5b1162c85abd41d1985107a4aeee71c73ad184e0
dennisDev77 <diniscamela5@gmail.com>
2025-03-28 16:03:25 +0100

    style: 💄 remove size h-72 to Banner
    I remove the h-72 to add h-60 to Banner
    
    BREAKING CHANGE: 🧨 remove

--- controllers/pages/Home/Banner.jsx
+++ controllers/pages/Home/Banner.jsx
@@ -3,8 +3,16 @@ import banner from '../../../../images/banner.jpg'
 import {FaSearch} from 'react-icons/fa'
 
 const Banner = () => {
+ const [query, setQuery]=React.useState()
+
+ function getSearch(e){
+  e.preventDefault() // no loading on submit
+    if(query){
+      window.location.href=`/movies/show/${query}/1`
+    }
+ }
   return (
-    <section className="relative bg-cover bg-center h-72 z-10 flex flex-col justify-center items-center" 
+    <section className="relative bg-cover bg-center h-60 z-10 flex flex-col justify-center items-center" 
             style={{ backgroundImage: `url(${banner})`}}>
 
             <div className={`bg-color_gray_500/55 h-full w-full absolute top-0 left-0 z-0`}></div>
@@ -12,15 +20,15 @@ const Banner = () => {
                 <div className=" text-gray-200 flex flex-col gap-4">
                     <div>    
                         <h1 className=" text-3xl font-normal md:text-5xl md:font-medium">Seja Bem Vindo<span className='text-ciano_escuro'>,</span></h1>
-                        <p className="text-xl md:text-2xl font-light">Aqui voce vai conhecer melhores Films, series e até mesmo Atores do momento </p>
+                        <p className="text-xl md:text-xl font-light">Aqui voce vai conhecer melhores Films, series e até mesmo Atores do momento </p>
                     </div>
 
                     {/* Form to submit movies, serie or actor to search */}
-                    <form className='relative flex justify-center items-center'>
-                        <input type='search' placeholder='Pesquisar por films, series ou nome de um autor' className='w-full py-2 px-6 pr-8 text-lg text-gray-600 font-light placeholder:text-base placeholder:text-gray-400 placeholder:font-light rounded-full'/>
+                    <form className='relative flex justify-center items-center' onSubmit={getSearch}>
+                        <input type='search' placeholder='Pesquisar por films, series ou nome de um autor' className='w-full py-2 px-6 pr-8 text-lg text-gray-600 font-light placeholder:text-base placeholder:text-gray-400 placeholder:font-light rounded-full' value={query} onChange={(e)=>setQuery(e.target.value)}/>
                         
                         <span className='absolute right-2 flex justify-center items-center'>
-                             <button className='text-xl font-light text-ciano_escuro'> <FaSearch/></button>
+                             <button className='text-xl font-light text-ciano_escuro'> <FaSearch/> </button>
                         </span>
                     </form>
                 </div>
